db6882d2d1b8a5858e19f1e71fc11b413fa6be47
Administrator <Administrator@4Y7ZC677F796ZML>
2015-07-24 15:50:23 +0800

    合并GITHUB

---
+++ 项目目录/企业平台/新浪/sae/dev/test/form/show.php
@@ -0,0 +1,35 @@
+<meta http-equiv="Content-Type" content="text/html; charset=utf8">//申明编码格式
+<?php
+
+$hostname = SAE_MYSQL_HOST_M.':'.SAE_MYSQL_PORT;
+$dbuser = SAE_MYSQL_USER;
+$dbpass = SAE_MYSQL_PASS;
+$dbname = SAE_MYSQL_DB;
+$con =  mysql_connect($hostname,$dbuser,$dbpass);//连接sql
+
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("app_equn", $con);//从sql中选择数据库
+
+$result = mysql_query("SELECT * FROM Persons");
+
+echo "<table border='1'>
+<tr>
+<th>姓</th>
+<th>名</th>
+</tr>";
+
+while($row = mysql_fetch_array($result))
+  {
+  echo "<tr>";
+  echo "<td>" . $row['FirstName'] . "</td>";
+  echo "<td>" . $row['LastName'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysql_close($con);
+?>

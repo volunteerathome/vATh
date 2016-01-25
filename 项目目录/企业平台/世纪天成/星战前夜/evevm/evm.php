ca861d92e20978707db7309bf20346626d9c10dd
gavin.h <859956006@qq.com>
2016-01-25 12:58:56 +0800

    Update

--- 项目目录/企业平台/世纪天成/星战前夜/evevm/evm.php
+++ 项目目录/企业平台/世纪天成/星战前夜/evevm/evm.php
@@ -1,11 +1,11 @@
 <html>
 <body>
 <meta http-equiv="Content-Type" content="text/html; charset=utf8">
-<form action="insert.php" method="post">
+<form action="insertevm.php" method="post">
 卖: <input type="text" name="sale" />
 买: <input type="text" name="buy" />
 ID: <input type="text" name="name" />
-发言:<input type="text" name=" word" />
+发言:<input type="text" name="word" />
 <input type="submit" />
 </form>
 
